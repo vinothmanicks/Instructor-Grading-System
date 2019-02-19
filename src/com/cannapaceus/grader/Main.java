b5e1937d9f3df115dcac7d4438f255055c08d1a1
Vinoth Kumar Manickavasagam <vinoth.manicks@gmail.com>
2019-02-18 19:35:59 -0600

    Created package

---
+++ src/com/cannapaceus/grader/Main.java
@@ -0,0 +1,17 @@
+/**
+ * @author - Charles Grady, James Shelton, Nicholas Bryant, Vinoth Kumar Manickavasagam
+ *
+ */
+package com.cannapaceus.grader;
+
+public class Main {
+
+    /**
+     * The program execution starts here
+     * @param args
+     */
+    public static void main (String[] args) {
+        // Do not remove below print statement
+        System.out.println("Instructor Grading Program");
+    }
+}
