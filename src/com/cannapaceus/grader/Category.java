cd8443fc6b5e44afcf9be1c29006304796147cf0
BiggestPotato <cjg0019@uah.edu>
2019-02-26 19:04:18 -0600

    Category and statistics are blank classes atm.
    Assignment, grade and student are almost done. Need to be reviewed.
    
    Can't determine if we need to retrieve references of the student
    and assignment that a grade has a reference to.

---
+++ src/com/cannapaceus/grader/Category.java
@@ -0,0 +1,11 @@
+package com.cannapaceus.grader;
+
+public class Category {
+
+    //Currently empty to resolve errors in other classes.
+    public Category(Category catCategory)
+    {
+
+    }
+
+}
--- src/com/cannapaceus/grader/Grade.java
+++ src/com/cannapaceus/grader/Grade.java
@@ -10,51 +10,161 @@ public class Grade {
     private boolean bDropped;
     private boolean bSubmitted;
 
-    //TODO:Fix student & assignment references
+    /**
+     * <h1>Grade Constructor</h1>
+     * Constructor for the assignment class.
+     * @param fGrade Grade for the assignment if grade is already known when creating
+     * @param stuStudent Student this grade is for. Is a reference and should be assigned at instantiation.
+     * @param aAssignment Assignemnt this grade is for. Is a reference and should be assigned at instantiation.
+     */
     public Grade(float  fGrade, Student stuStudent, Assignment aAssignment) {
         this.fGrade = fGrade;
+
+        //Should not be able to change these variables
         this.stuStudent = stuStudent;
         this.aAssignment = aAssignment;
+
+        bOverdue = false;
+        bMissing = false;
+        bDropped = false;
+        bSubmitted = false;
     }
 
+    //Setter functions
+    /**
+     * Setter for the Grade class's grade
+     * @param fGrade Float of the desired grade for this instance
+     */
     public void setGrade(float fGrade)
     {
         this.fGrade = fGrade;
     }
+
+    /**
+     * Setter for the Grade class's overdue flag
+     * @param bOverdue
+     */
     public void setOverdue(boolean bOverdue)
     {
         this.bOverdue = bOverdue;
     }
+
+    /**
+     * Setter for the Grade class's missing flag
+     * @param bMissing
+     */
     public void setMissing(boolean bMissing)
     {
         this.bMissing = bMissing;
     }
+
+    /**
+     * Setter for the Grade class's dropped flag
+     * @param bDropped
+     */
     public void setDropped(boolean bDropped)
     {
         this.bDropped = bDropped;
     }
+
+    /**
+     * Setter for the Grade class's submitted flag
+     * @param bSubmitted
+     */
     public void setSubmitted(boolean bSubmitted)
     {
         this.bSubmitted = bSubmitted;
     }
+
+    //Getter functions
+
+    /**
+     * Getter for the Grade class's grade float
+     * @return Copy of the grade float
+     */
     public float getGrade()
     {
         return fGrade;
     }
+
+    /**
+     * Setter for the Grade class's overdue flag
+     * @return Copy of the overdue flag status
+     */
     public boolean getOverdue()
     {
         return bOverdue;
     }
+
+    /**
+     * Setter for the Grade class's missing flag
+     * @return Copy of the missing flag's status
+     */
     public boolean getMissing()
     {
         return bMissing;
     }
+
+    /**
+     * Setter for the Grade class's dropped flag
+     * @return Copy of the dropped flag's status
+     */
     public boolean getDropped()
     {
         return bDropped;
     }
+
+    /**
+     * Setter for the Grade class's submitted flag
+     * @return submitted flag's status
+     */
     public boolean getSubmitted()
     {
         return bSubmitted;
     }
+
+    /**
+     * Getter for a copy of the student this grade is for
+     * @return Copy of Grade's student variable
+     */
+    public Student getStudentCopy()
+    {
+        Student copyStudent = new Student(stuStudent);
+        return copyStudent;
+    }
+
+    /**
+     * Getter for a copy of the assignment this grade is for
+     * @return Copy of the Grade's assignment variable
+     */
+    public Assignment getAssignmentCopy()
+    {
+        Assignment aAssignmentCopy = new Assignment(aAssignment);
+        return aAssignmentCopy;
+    }
+
+
+    //Potential functions if we need to get references
+
+    /**
+     * Getter for a reference to the assignment this grade is for
+     * @return Reference of the grade's assignment variable
+     */
+    /*
+    public Assignment getAssignmentReference()
+    {
+        return aAssignment;
+    }
+    */
+
+    /**
+     * Getter for a reference to the the student this grade is for
+     * @return Reference of the grade's student variable
+     */
+    /*
+    public Student getStudentReference()
+    {
+        return stuStudent;
+    }
+    */
 }
