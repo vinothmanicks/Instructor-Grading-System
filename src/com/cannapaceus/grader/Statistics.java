1109bf7584574aa8768aac9e692ab75fa2e2fbdc
BiggestPotato <cjg0019@uah.edu>
2019-03-04 19:17:33 -0600

    Statistics fleshed out.
    TODO: Implement calculation functions.
    
    No set functions has been implemented for stats. The values should only be calculated from passed grades.

--- src/com/cannapaceus/grader/Statistics.java
+++ src/com/cannapaceus/grader/Statistics.java
@@ -2,5 +2,53 @@ package com.cannapaceus.grader;
 
 public class Statistics {
 
-    //Currently empty to remove errors in the other classes
+    private float fMean;
+    private float fMedian;
+    private float fMode;
+    private float fStandardDev;
+
+    public Statistics()
+    {
+
+    }
+
+    public float getMean()
+    {
+        return this.fMean;
+    }
+
+    public float getMedian()
+    {
+        return this.fMedian;
+    }
+
+    public float getMode()
+    {
+        return this.fMode;
+    }
+
+    public float getStandardDev()
+    {
+        return this.fStandardDev;
+    }
+
+    public void calculateMean()
+    {
+        //TODO: Implement calculations
+    }
+
+    public void calculateMedian()
+    {
+
+    }
+
+    public void calculateMode()
+    {
+
+    }
+
+    public void calculateStandardDev()
+    {
+
+    }
 }
