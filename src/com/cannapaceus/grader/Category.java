4784160f04ef344e2fa4677d212df5129ee4353c
James Shelton <jes0030@uah.edu>
2019-03-13 14:59:00 +0000

    Uses most recent Category class as of 03/11

--- src/com/cannapaceus/grader/Category.java
+++ src/com/cannapaceus/grader/Category.java
@@ -1,11 +1,63 @@
 package com.cannapaceus.grader;
 
+import java.util.ArrayList;
+
 public class Category {
 
-    //Currently empty to resolve errors in other classes.
-    public Category(Category catCategory)
-    {
+    //The name of the category
+    private String sCategoryName;
+    //The weight assigned to this category
+    private float fCategoryWeight;
+    //The list of assignments assigned to this category
+    private ArrayList<Assignment> lCatAssignments;
 
+    public Category(String sCategoryName, float fCategoryWeight)
+    {
+        this.sCategoryName = sCategoryName;
+        this.fCategoryWeight = fCategoryWeight;
+        lCatAssignments = new ArrayList<>();
     }
 
+    public Category(Category catCategory)
+    {
+        this.sCategoryName = catCategory.getName();
+        this.fCategoryWeight = catCategory.getWeight();
+        this.lCatAssignments = new ArrayList<>(catCategory.getAssignments());
+    }
+
+    public String getName()
+    {
+        String copyString = new String(sCategoryName);
+        return copyString;
+    }
+
+    public float getWeight()
+    {
+        return fCategoryWeight;
+    }
+
+    public ArrayList<Assignment> getAssignments()
+    {
+        return new ArrayList<>(lCatAssignments);
+    }
+
+    public void addAssignment(Assignment aAssignment)
+    {
+        lCatAssignments.add(aAssignment);
+    }
+
+    public void removeAssigment(Assignment aAssignment)
+    {
+        lCatAssignments.remove(aAssignment);
+    }
+
+    public void setWeight(float fCategoryWeight)
+    {
+        this.fCategoryWeight = fCategoryWeight;
+    }
+
+    public void setCategoryName(String categoryName)
+    {
+        sCategoryName = new String(categoryName);
+    }
 }
