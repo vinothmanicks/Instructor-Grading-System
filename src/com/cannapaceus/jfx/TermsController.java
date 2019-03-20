6deea0c3e65f87a3a261c000b3a2fdcc0fa514c2
NicholasBryant <nabryant299@gmail.com>
2019-03-19 19:03:51 -0500

    Added courses as collapsible sublists of terms in TermsView

--- src/com/cannapaceus/jfx/TermsController.java
+++ src/com/cannapaceus/jfx/TermsController.java
@@ -1,14 +1,18 @@
 package com.cannapaceus.jfx;
 
+import com.cannapaceus.grader.Course;
 import com.cannapaceus.grader.DBService;
 import com.cannapaceus.grader.Term;
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXListView;
+import de.jensd.fx.glyphs.fontawesome.FontAwesomeIconView;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.geometry.Pos;
+import javafx.scene.Node;
+import javafx.scene.control.Control;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListCell;
 import javafx.scene.control.Separator;
@@ -16,46 +20,18 @@ import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
 
+import javax.swing.*;
 import java.util.ArrayList;
-
-class TermCell extends ListCell<Term> {
-    VBox vbox = new VBox();
-    HBox hbox = new HBox();
-    Label label = new Label("");
-    Pane hbPane = new Pane();
-    JFXButton btnDelete = new JFXButton("Delete");
-    JFXButton btnAdd = new JFXButton("Add Course");
-
-    public TermCell() {
-        super();
-
-        hbox.getChildren().addAll(label, hbPane, btnDelete);
-        hbox.setHgrow(hbPane, Priority.ALWAYS);
-        vbox.getChildren().addAll(hbox, btnAdd);
-    }
-
-    public void updateItem(Term t, boolean empty) {
-        super.updateItem(t, empty);
-        setText(null);
-        setGraphic(null);
-
-        if (t != null && !empty) {
-            label.setText(t.getSeason().toString() + " " + t.getYear());
-            setGraphic(vbox);
-        }
-    }
-}
+import java.util.HashMap;
 
 public class TermsController {
     ScreenController sc = null;
     DBService db = null;
 
-//    ObservableList<Term> lTerms;
     ArrayList<Term> lTerms;
-
-    @FXML
-    private JFXListView<Term> lvTerms;
 
     @FXML
     private VBox vbTerms;
@@ -66,32 +42,9 @@ public class TermsController {
         sc = ScreenController.getInstance();
         db = DBService.getInstance();
 
-//        lTerms = FXCollections.observableArrayList(db.retrieveTerms());
-
-//        lvTerms.setItems(lTerms);
-//        lvTerms.setCellFactory(param -> new TermCell());
-
         lTerms = db.retrieveTerms();
 
-        for (Term t : lTerms) {
-            Separator tempSep = new Separator();
-
-            VBox tempVB = new VBox();
-            HBox tempHB = new HBox();
-            tempHB.setAlignment(Pos.CENTER_LEFT);
-            Label tempLabel = new Label(t.getSeason().toString() + " " + t.getYear());
-            tempLabel.setAlignment(Pos.CENTER_LEFT);
-            Pane tempPane = new Pane();
-            JFXButton tempDelete = new JFXButton("Delete");
-            tempDelete.setStyle("-fx-background-color: #f44336; -fx-text-fill: white;");
-            tempDelete.setButtonType(JFXButton.ButtonType.RAISED);
-
-            tempHB.getChildren().addAll(tempLabel, tempPane, tempDelete);
-            tempHB.setHgrow(tempPane, Priority.ALWAYS);
-            tempVB.getChildren().add(tempHB);
-
-            vbTerms.getChildren().addAll(tempSep, tempVB);
-        }
+        createTermList();
     }
 
     public void handleSubmitButtonAction(ActionEvent actionEvent) {
@@ -99,6 +52,144 @@ public class TermsController {
     }
 
     private void createTermList() {
+        for (Term t : lTerms) {
+            Separator tempSep = new Separator();
 
+            VBox tempVB = new VBox();
+            tempVB.setSpacing(10.0);
+            HBox tempHB = new HBox();
+            tempHB.setAlignment(Pos.CENTER_LEFT);
+
+            Label tempLabel = new Label(t.getSeason().toString() + " " + t.getYear());
+            tempLabel.setAlignment(Pos.CENTER_LEFT);
+
+            Pane tempPane = new Pane();
+
+            FontAwesomeIconView tempFA = new FontAwesomeIconView();
+            tempFA.setGlyphName("TRASH_ALT");
+            tempFA.setGlyphSize(20);
+            tempFA.setGlyphStyle("-fx-fill: grey;");
+
+            JFXButton tempDelete = new JFXButton("");
+            tempDelete.setAlignment(Pos.BASELINE_CENTER);
+            tempDelete.setGraphic(tempFA);
+            tempDelete.setStyle("-fx-cursor: hand;");
+            tempDelete.setRipplerFill(Color.WHITE);
+            tempDelete.setButtonType(JFXButton.ButtonType.FLAT);
+
+            tempFA = new FontAwesomeIconView();
+            tempFA.setGlyphName("CHEVRON_RIGHT");
+            tempFA.setGlyphSize(20);
+            tempFA.setGlyphStyle("-fx-fill: grey;");
+
+            JFXButton tempExpand = new JFXButton("");
+            tempExpand.setAlignment(Pos.BASELINE_CENTER);
+            tempExpand.setGraphic(tempFA);
+            tempExpand.setStyle("-fx-cursor: hand;");
+            tempExpand.setRipplerFill(Color.WHITE);
+            tempExpand.setButtonType(JFXButton.ButtonType.FLAT);
+            tempExpand.setOnAction((event -> expand(event)));
+
+            VBox vbCourses = new VBox();
+            vbCourses.setVisible(false);
+            vbCourses.setManaged(false);
+
+            for (Course c : t.getCourses()) {
+                HBox hbCourse = new HBox();
+                hbCourse.setSpacing(10.0);
+                hbCourse.setStyle("-fx-padding: 10,10,10,10;");
+
+                Label lblCourseName = new Label(c.getCourseName());
+                Label lblCourseDept = new Label(c.getDepartment());
+
+                Pane spanPane = new Pane();
+
+                tempFA = new FontAwesomeIconView();
+                tempFA.setGlyphName("TRASH_ALT");
+                tempFA.setGlyphSize(20);
+                tempFA.setGlyphStyle("-fx-fill: grey;");
+
+                JFXButton courseDelete = new JFXButton("");
+                courseDelete.setAlignment(Pos.BASELINE_CENTER);
+                courseDelete.setGraphic(tempFA);
+                courseDelete.setStyle("-fx-cursor: hand;");
+                courseDelete.setRipplerFill(Color.LIGHTGREY);
+                courseDelete.setButtonType(JFXButton.ButtonType.FLAT);
+
+                hbCourse.getChildren().addAll(lblCourseName, lblCourseDept, spanPane, courseDelete);
+                hbCourse.setHgrow(spanPane, Priority.ALWAYS);
+                vbCourses.getChildren().add(hbCourse);
+            }
+
+            tempHB.getChildren().addAll(tempLabel, tempPane, tempDelete, tempExpand);
+            tempHB.setHgrow(tempPane, Priority.ALWAYS);
+            tempVB.getChildren().addAll(tempHB, vbCourses);
+
+            vbTerms.getChildren().addAll(tempSep, tempVB);
+        }
+    }
+
+    private void expand(ActionEvent e) {
+        JFXButton t = (JFXButton) e.getTarget();
+
+        HBox p = (HBox) t.getParent();
+
+        VBox vbTerm = (VBox) p.getParent();
+
+        p.getChildren().remove(t);
+
+        FontAwesomeIconView tempFA = new FontAwesomeIconView();
+        tempFA.setGlyphName("CHEVRON_DOWN");
+        tempFA.setGlyphSize(20);
+        tempFA.setGlyphStyle("-fx-fill: grey;");
+
+        JFXButton tempCollapse = new JFXButton("");
+        tempCollapse.setAlignment(Pos.BASELINE_CENTER);
+        tempCollapse.setGraphic(tempFA);
+        tempCollapse.setStyle("-fx-cursor: hand;");
+        tempCollapse.setRipplerFill(Color.WHITE);
+        tempCollapse.setButtonType(JFXButton.ButtonType.FLAT);
+        tempCollapse.setOnAction((event -> collapse(event)));
+
+        for (Node n : vbTerm.getChildren()) {
+            if (n instanceof VBox) {
+                n.setVisible(true);
+                n.setManaged(true);
+            }
+        }
+
+        p.getChildren().addAll(tempCollapse);
+    }
+
+    private void collapse(ActionEvent e) {
+        JFXButton t = (JFXButton) e.getTarget();
+
+        HBox p = (HBox) t.getParent();
+
+        VBox vbTerm = (VBox) p.getParent();
+
+        p.getChildren().remove(t);
+
+        FontAwesomeIconView tempFA = new FontAwesomeIconView();
+        tempFA.setGlyphName("CHEVRON_RIGHT");
+        tempFA.setGlyphSize(20);
+        tempFA.setGlyphStyle("-fx-fill: grey;");
+
+        JFXButton tempCollapse = new JFXButton("");
+        tempCollapse.setAlignment(Pos.BASELINE_CENTER);
+        tempCollapse.setGraphic(tempFA);
+        tempCollapse.setStyle("-fx-cursor: hand;");
+        tempCollapse.setRipplerFill(Color.WHITE);
+        tempCollapse.setButtonType(JFXButton.ButtonType.FLAT);
+        tempCollapse.setOnAction((event -> expand(event)));
+
+        for (Node n : vbTerm.getChildren()) {
+            if (n instanceof VBox) {
+                n.setVisible(false);
+                n.setManaged(false);
+            }
+        }
+
+        p.getChildren().add(tempCollapse);
     }
 }
