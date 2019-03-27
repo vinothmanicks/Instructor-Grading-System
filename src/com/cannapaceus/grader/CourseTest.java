5e13682d7c94f89f7d724cd034af4afe50091d29
Vinoth Kumar Manickavasagam <vinoth.manicks@gmail.com>
2019-03-26 23:41:29 -0500

    Added Unit Tests for Category and Assignment

---
+++ src/com/cannapaceus/grader/CourseTest.java
@@ -0,0 +1,109 @@
+package com.cannapaceus.grader;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CourseTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void addStudent() {
+    }
+
+    @Test
+    void addAssignment() {
+    }
+
+    @Test
+    void addCategory() {
+    }
+
+    @Test
+    void addGrade() {
+    }
+
+    @Test
+    void removeStudent() {
+    }
+
+    @Test
+    void removeAssignment() {
+    }
+
+    @Test
+    void removeCategory() {
+    }
+
+    @Test
+    void removeGrade() {
+    }
+
+    @Test
+    void archive() {
+    }
+
+    @Test
+    void populateAverages() {
+    }
+
+    @Test
+    void calculateStats() {
+    }
+
+    @Test
+    void getDBID() {
+    }
+
+    @Test
+    void getCourseName() {
+    }
+
+    @Test
+    void getCourseID() {
+    }
+
+    @Test
+    void getDepartment() {
+    }
+
+    @Test
+    void getlStudents() {
+    }
+
+    @Test
+    void getlAssignments() {
+    }
+
+    @Test
+    void getlCategories() {
+    }
+
+    @Test
+    void getlGrades() {
+    }
+
+    @Test
+    void setDBID() {
+    }
+
+    @Test
+    void setCourseName() {
+    }
+
+    @Test
+    void setCourseID() {
+    }
+
+    @Test
+    void setDepartment() {
+    }
+
+    @Test
+    void compareTo() {
+    }
+}
